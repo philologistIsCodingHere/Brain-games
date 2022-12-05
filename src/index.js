99c67ef3d4f76ca018d9fcd34d6189c59df465d8
philologistIsCodingHere <ari.msnkv@gmail.com>
2022-12-05 21:56:21 +0300

    delete comment

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ const gamesLogic = (game) => {
   console.log(`Hello, ${name}!`);
   console.log(game.task);
   for (let i = 1; i <= 3; i += 1) {
-    const [question, result] = game.getQuestionAndResult(); // question, result = ['16 + 25', '41'];
+    const [question, result] = game.getQuestionAndResult();
     console.log(`Question: ${question}`);
     const answer = readlineSync.question('Your answer: ');
     if (answer === result) {
