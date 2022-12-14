54acb6fdbe25e73556fc4c981a10080289e6aa4d
philologistIsCodingHere <ari.msnkv@gmail.com>
2022-12-14 21:12:09 +0300

    update index.js

--- src/index.js
+++ src/index.js
@@ -1,11 +1,13 @@
 import readlineSync from 'readline-sync';
 
+const ROUNDS_COUNT = 3;
+
 const gamesLogic = (game) => {
   console.log('Welcome to the Brain Games!');
   const name = readlineSync.question('May I have your name? ');
   console.log(`Hello, ${name}!`);
   console.log(game.task);
-  for (let i = 1; i <= 3; i += 1) {
+  for (let i = 1; i <= ROUNDS_COUNT; i += 1) {
     const [question, result] = game.getQuestionAndResult();
     console.log(`Question: ${question}`);
     const answer = readlineSync.question('Your answer: ');
