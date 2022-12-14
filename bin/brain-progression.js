dad6c65be4a5e6b5690d7fdb9e0491743d4f0291
philologistIsCodingHere <ari.msnkv@gmail.com>
2022-12-14 21:11:15 +0300

    update brain-progression.js

--- bin/brain-progression.js
+++ bin/brain-progression.js
@@ -1,5 +1,5 @@
 #!/usr/bin/env node
 import gamesLogic from '../src/index.js';
-import * as game from '../games/progression.js';
+import * as game from '../src/games/progression.js';
 
 gamesLogic(game);
